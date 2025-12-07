33e8f7440c21a1c1d6374e9461b72e7adb155284
Kanyicska Nándor <kanyicskan@gmail.com>
2025-12-07 20:36:22 +0100

    Remove local gh-pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -6,3 +6,13 @@ export default defineConfig({
 	plugins: [preact()],
 	base: '/quiz-game/',
 });
+
+{
+  "scripts": {
+    "dev": "vite",
+    "build": "vite build",
+    "preview": "vite preview"
+  }
+}
+
+
