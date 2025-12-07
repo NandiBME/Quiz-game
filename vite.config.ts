63d735c189f8af8e2f658f4035ea01310ae982c5
Kanyicska Nándor <kanyicskan@gmail.com>
2025-12-07 22:55:01 +0100

    jkp

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,11 @@ import preact from '@preact/preset-vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	base: './',
+	build: {
+		outDir: 'docs',
+	},
 	plugins: [preact()],
-	base: '/Quiz-game/',
 });
 
 
