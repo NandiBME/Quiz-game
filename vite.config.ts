e42be5f399d6a6103c198f48f448d367816d1588
Kanyicska Nándor <kanyicskan@gmail.com>
2025-12-07 20:37:50 +0100

    Fix base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,9 @@ import preact from '@preact/preset-vite';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [preact()],
-	base: '/quiz-game/',
+	base: '/Quiz-game/',
 });
 
-{
-  "scripts": {
-    "dev": "vite",
-    "build": "vite build",
-    "preview": "vite preview"
-  }
-}
+
 
 
